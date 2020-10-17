e419ad31d74af3612ba3ee836cb41dd602b9a543
SinkLineP <popovartem505@gmail.com>
2020-10-17 18:40:26 +0300

    add /docs

---
+++ docs/precache-manifest.c7d57786863ff7455443948836c40a4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fccc5f7b5c35aef771d89f229171184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600b1595cc97d81e0ebf",
+    "url": "/static/css/main.afa27e98.chunk.css"
+  },
+  {
+    "revision": "fcd3dbf4846fd15b89a8",
+    "url": "/static/js/2.5e4cdcd9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5e4cdcd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600b1595cc97d81e0ebf",
+    "url": "/static/js/main.78da9047.chunk.js"
+  },
+  {
+    "revision": "e78ae9a9f6d8bf2a13ec",
+    "url": "/static/js/runtime-main.b8975f5f.js"
+  },
+  {
+    "revision": "b768b2c77c2a6701e738675d2778ff85",
+    "url": "/static/media/Director.b768b2c7.jpg"
+  }
+]);
